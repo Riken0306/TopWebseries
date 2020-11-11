c73f4d09bf15e30d4ab5e385a7c58f6585fca8e1
Riken0306 <riken0306@gmail.com>
2020-11-11 10:11:29 +0530

    First commit to Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Card from './Card'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card 
+      imgsrc="https://wallpapercave.com/wp/wp4056399.jpg" 
+      title="A Netflix Original Series"
+      sname="DARK"
+      link="https://www.netflix.com/in/title/80100172"
+      />
+      <Card 
+      imgsrc="https://www.businessinsider.in/thumb/msid-78571585,width-600,resizemode-4,imgsize-352843/thelife/personalities/news/what-was-the-harshad-mehta-scam-1992-checkout-this-story-on-sonyliv/harshad-mehta.jpg" 
+      title="Sony Liv Series"
+      sname="Scam 1992"
+      link="https://www.sonyliv.com/shows/scam-1992-the-harshad-mehta--1700000292"
+      />
+      <Card 
+      imgsrc="https://upload.wikimedia.org/wikipedia/en/5/5c/Inside_Edge.jpeg" 
+      title="An amazon Prime video"
+      sname="INSIDE EDGE"
+      link="https://www.amazon.com/Inside-Edge-Season-English-Subtitles/dp/B073PFJPJT"
+      />
+    </>
   );
 }
 
 export default App;
+//https://wallpapercave.com/wp/wp4056399.jpg
+//A Netflix Original Series
+//DARK
+//https://www.netflix.com/in/title/80100172
